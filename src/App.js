d278a25d70afa577e21d76de142950a28b3fec50
haesol <haesol.k01@gmail.com>
2022-06-13 18:04:42 +0900

    5th commit -6일차 완료

--- src/App.js
+++ src/App.js
@@ -9,11 +9,12 @@ class App extends Component {
     super(props);
     this.state = {
       mode: "read",
+      selected_content_id: 2,
       Subject: {
         title: "WEB",
         sub: "World Wide Web!!",
       },
-      Welcome: {title:"welcome", desc:"Hello, react!!"},
+      Welcome: { title: "welcome", desc: "Hello, react!!" },
       Contents: [
         { id: 1, title: "HTML", desc: "HTML is information" },
         { id: 2, title: "CSS", desc: "CSS is for Design" },
@@ -22,33 +23,43 @@ class App extends Component {
     };
   }
   render() {
-    console.log('App render');
-    var _title, _desc = null;
-    if(this.state.mode === 'welcome'){
+    console.log("App render");
+    var _title,
+      _desc = null;
+    if (this.state.mode === "welcome") {
       _title = this.state.Welcome.title;
       _desc = this.state.Welcome.desc;
-    }else if(this.state.mode === 'read'){
-      _title = this.state.Contents[0].title;
-      _desc = this.state.Contents[0].desc;
+    } else if (this.state.mode === "read") {
+      var i = 0;
+      while (i < this.state.Contents.length) {
+        var data = this.state.Contents[i];
+        if (data.id === this.state.selected_content_id) {
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i = i + 1;
+      }
     }
     return (
       <div className="App">
-        {/* <Subject
+        <Subject
           title={this.state.Subject.title}
           sub={this.state.Subject.sub}
-        /> */}
-      <header>
-        <h1>
-          <a href="/" onClick={ function (e) {
-            console.log(e);
-            e.preventDefault();
-            this.setState({mode: "welcome"});
-          }.bind(this)}>{this.state.Subject.title}</a>
-        </h1>
-        {this.state.Subject.sub}
-      </header>
+          onChangePage={function () {
+            this.setState({ mode: "welcome" });
+          }.bind(this)}
+        />
         <Subject title="react" sub="for UI" />
-        <TOC data={this.state.Contents} />
+        <TOC
+          onChangePage={function (id) {
+            this.setState({ 
+              mode: "read",
+              selected_content_id: Number(id)
+             });
+          }.bind(this)}
+          data={this.state.Contents}
+        />
         <Content title={_title} desc={_desc} />
       </div>
     );
